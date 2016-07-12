4625ad2bc4a97c9a6b62142252cf7a25378a470b
Justin <justin@crystalknows.com>
2016-07-11 20:00:20 -0500

    added YT icon to footer

--- assets/footer.php
+++ assets/footer.php
@@ -5,4 +5,7 @@
 	<div>
 		<a href="https://twitter.com/justinmulvs" target="_blank"><img src="http://www.justinmulvaney.com/assets/images/twitter.png" class="twitter-icon"></a>
 	</div>
+	<div>
+		<a href="https://www.youtube.com/channel/UC-PgnS4xNhJxRHatlo8sZTw" target="_blank"><img src="http://www.justinmulvaney.com/assets/images/youtube.png" class='youtube-icon'>
+	</div>
 </footer>
