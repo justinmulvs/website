9d79a2ee136c177774cac9c3ace77d52ca4e4e8e
Justin <justin@crystalknows.com>
2016-11-15 11:49:50 -0600

    Entire books update

--- books.php
+++ books.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Leave the above for bootstrap -->
+
 	<title>Books | Justin Mulvaney</title>
 	<meta name="description" content="Justin Mulvaney | I like to read a lot of books. I share my notes from them here.">
-	<meta charset="utf-8">
+
 	<!--styles go here-->
-	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700|Lato:400,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
@@ -13,7 +18,6 @@
 	<style type="text/css">
 		#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; max-width: 80%; margin: auto;}
 	</style>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Google Analytics Tracking-->
 	<script>
@@ -27,122 +31,133 @@
 	</script>
 </head>
 
-<body id="books">
+<body>
 
-	<div class="container group" id="books-container">
-		<div id="books-overview">
-			<h1>Book Notes</h1>
-			<p>I read a lot of nonfiction, and take detailed notes for most books. I like to think of these as <b>sparknotes for books you give a shit about.</b> Some things to know as you browse:</p>
-			<ol>
-				<li>If it's listed here, I think it's worth reading.</li>
-				<li>They're categorized, generally with the books I recommend the most at the top.</li>
-				<li>There aren't notes uploaded for all of them. If you see a book you'd like notes for shoot me a tweet or an email!</li>
-			</ol>
-			<p>If I haven't given it away already, I'm a book nerd. <b>I would love to hear from you regarding top book recs, thoughts or questions on any of the books/notes here, or requests regarding books you'd like me to read and take notes on.</b> If you want to be updated when I release new notes, you can <a href="http://eepurl.com/bq4RMz"><b>join the email list</b></a>.<br><br>
-			Onto the books!
-			 </p>
+	<div class="container-fluid">
+		<div class="row" id="site-header">
+			<div class="col-xs-12 col-sm-6">
+				<a href="index.php"><h3>Justin Mulvaney</h3>
+				<h4>Growth | Data Analytics | Product Strategy</h4></a>
+			</div>
+
+			<div class="col-xs-12 col-sm-6" id="books-icons">
+				<a href="https://angel.co/justin-mulvaney" target="_blank"><img src="assets/images/al_icon.png" alt="AngelList Icon" class="al-icon"></a>
+				<a href="https://www.linkedin.com/in/justinmulvaney" target="_blank"><img src="assets/images/li_icon.png" alt="LinkedIn Icon" class="icon"></a>
+				<a href="https://twitter.com/justinmulvs" target="_blank"><img src="assets/images/twitter_icon.png" alt="Twitter Icon" class="icon"></a>
+				<a href="mailto:justin.mulvaney@gmail.com" target="_blank"><img src="assets/images/email_icon.png" alt="Email Icon" class="icon"></a>
+			</div>
 		</div>
 
-		<div id="top">
-			<p><span class="underline">My Top 3</span></p>
-			<ul class="book-list">
-				<li><a href="books/seeking-wisdom-from-darwin-to-munger" target="_blank"><b>Seeking Wisdom: From Darwin to Munger</b></a> -Peter Bevelin</li>
-				<li><b>Thinking, Fast and Slow</b> -Daniel Kahneman</li>
-				<li><b>The 7 Habits of Highly Effective People</b> -Stephen R. Covey</li>
-			</ul>
+		</div>
+	</div>
+
+	<div class="container books">
+		<div class="row">
+			<div class="col-xs-12 col-md-10 col-md-offset-1 books-overview">
+				<h1>Book Notes</h1>
+				<p>I read a lot of nonfiction, and take detailed notes for most books I read. I think of these as sparknotes for books you give a shit about. Some things to know:</p>
+				<ol>
+					<li>If it's listed here, I think it's worth reading.</li>
+					<li>They're categorized, generally with the books I recommend the most at the top.</li>
+					<li>There aren't notes uploaded for all of them. If you see a book you'd like notes for shoot me a tweet or an email.</li>
+				</ol>
+				<p>You can see a far more complete list of books I've read, including those I'm currently reading, <a href="https://www.goodreads.com/justinmulvaney" target="_blank">here</a>. If you want to be updated when I release new notes, <a href="http://eepurl.com/bq4RMz"><b>join the email list</b></a>. If you want to talk books feel free to reach out!<br><br>
+				Onto the books!
+				 </p>
+			</div>
+
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+				<p><u>My Top 3</u></p>
+				<ul>
+					<li><a href="books/seeking-wisdom-from-darwin-to-munger" target="_blank"><b>Seeking Wisdom: From Darwin to Munger</b></a> -Peter Bevelin</li>
+					<li><b>Thinking, Fast and Slow</b> -Daniel Kahneman</li>
+					<li><b>The 7 Habits of Highly Effective People</b> -Stephen R. Covey</li>
+				</ul>
+
+				<p><u>Psychology</u></p>
+				<ul>
+					<li><b>Influence</b> -Robert Cialdini</li>
+					<li><b><a href="books/predictably-irrational" target="_blank">Predictably Irrational</a></b> -Dan Ariely</li>
+					<li><b>Flow</b> -Mihaly Csikszentmihalyi</li>
+					<li><b>Drive</b> -Daniel Pink</li>
+					<li><b>The Paradox of Choice</b> -Barry Schwartz</li>
+					<li><b>The Power of Habit</b> -Charles Duhigg</li>
+					<li><b>Positive Personality Profiles</b> -Robert A. Rohm</li>
+				</ul>
+
+				<p><u>Happiness/Life</u></p>
+				<ul class="book-list">
+					<li><b>The Happiness Hypothesis</b> -Jonathan Haidt</li>
+					<li><b>The Power of Full Engagement</b> -Jim Loehr and Tony Schwarz</li>
+					<li><b><a href="books/essentialism" target="_blank">Essentialism: The Disciplined Pursuit of Less</a></b> -Greg McKeown</li>
+					<li><b>SuperBetter: A Revolutionary Approach to Getting Stronger, Happier, Braver and More Resilient</b> -Jane McGonigal</li>
+				</ul>
+
+				<p><u>Philosophy/Life</u></p>
+				<ul class="book-list">
+					<li><b><a href="books/a-guide-to-the-good-life" target="_blank">A Guide to The Good Life</a></b> -William Irvine</li>
+					<li><b>Lying</b> -Sam Harris</li>
+				</ul>
+
+				<p><u>General Business</u></p>
+				<ul class="book-list">
+					<li><b>Zero to One</b> -Peter Thiel</li>
+					<li><b>Positioning</b> -Al Ries and Jack Trout</li>
+					<li><b>Tribes</b> -Seth Godin</li>
+					<li><b><a href="books/never-eat-alone" target="_blank">Never Eat Alone</a></b> -Keith Ferrazzi</li>
+					<li><b>How to Win Friends and Influence People</b> -Dale Carnegie</li>
+				</ul>
+
+				<p><u>Misc. Others</u> (They're still great books!)</p>
+				<ul class="book-list">
+					<li><b>The Selfish Gene</b> -Ricard Dawkins</li>
+					<li><b><a href="books/in-defense-of-food" target="_blank">In Defense of Food</a></b> -Michael Pollan</li>
+					<li><b>Don't Make Me Think</b> -Steve Krug</li>
+					<li><b>Rocket Surgery Made Easy</b> -Steve Krug</li>
+				</ul>
+			</div>
 		</div>
 
-		<div class="books left">
-			<p><span class="underline">Psychology</span></p>
-			<ul class="book-list">
-				<li><b>Influence</b> -Robert Cialdini</li>
-				<li><b><a href="books/predictably-irrational" target="_blank">Predictably Irrational</a></b> -Dan Ariely</li>
-				<li><b>Flow</b> -Mihaly Csikszentmihalyi</li>
-				<li><b>Drive</b> -Daniel Pink</li>
-				<li><b>The Paradox of Choice</b> -Barry Schwartz</li>
-				<li><b>The Power of Habit</b> -Charles Duhigg</li>
-				<li><b>Positive Personality Profiles</b> -Robert A. Rohm</li>
-			</ul>
+		<!-- Begin MailChimp Signup Form -->
+		<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			#mc_embed_signup{background:#000; clear:left; font:14px Helvetica,Arial,sans-serif; margin-top: 20px; border: 2px solid grey; }
+			/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+		</style>
+		<div id="mc_embed_signup">
+		<form action="//justinmulvaney.us11.list-manage.com/subscribe/post?u=225664e6acb42af32191318f2&amp;id=0615841a9b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<div id="mc_embed_signup_scroll">
+			<h2>Want to be updated as the list grows? Join the mailing list!</h2>
+		<div class="indicates-required"><u class="asterisk">*</u> indicates required</div>
+		<div class="mc-field-group">
+			<label for="mce-EMAIL">Email Address  <u class="asterisk">*</u>
+		</label>
+			<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 		</div>
-
-		<div class="books right">
-			<p><span class="underline">Happiness/Life</span></p>
-			<ul class="book-list">
-				<li><b>The Happiness Hypothesis</b> -Jonathan Haidt</li>
-				<li><b>The Power of Full Engagement</b> -Jim Loehr and Tony Schwarz</li>
-				<li><b><a href="books/essentialism" target="_blank">Essentialism: The Disciplined Pursuit of Less</a></b> -Greg McKeown</li>
-				<li><b>SuperBetter: A Revolutionary Approach to Getting Stronger, Happier, Braver and More Resilient</b> -Jane McGonigal</li>
-			</ul>
+		<div class="mc-field-group">
+			<label for="mce-FNAME">First Name  <u class="asterisk">*</u>
+		</label>
+			<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
 		</div>
-
-		<div class="books left" >
-			<p><span class="underline">Philosophy/Life</span></p>
-			<ul class="book-list">
-				<li><b><a href="books/a-guide-to-the-good-life" target="_blank">A Guide to The Good Life</a></b> -William Irvine</li>
-				<li><b>Lying</b> -Sam Harris</li>
-			</ul>
+		<div class="mc-field-group input-group">
+		    <strong>Justin Mulvaney's Email List </strong>
+		    <ul><li><input type="checkbox" value="1" name="group[13545][1]" id="mce-group[13545]-13545-0"><label for="mce-group[13545]-13545-0"> General Updates</label></li>
+		<li><input type="checkbox" value="2" name="group[13545][2]" id="mce-group[13545]-13545-1"><label for="mce-group[13545]-13545-1"> Daily Blog</label></li>
+		</ul>
 		</div>
-
-		<div class="books right">
-			<p><span class="underline">General Business</span></p>
-			<ul class="book-list">
-				<li><b>Zero to One</b> -Peter Thiel</li>
-				<li><b>Positioning</b> -Al Ries and Jack Trout</li>
-				<li><b>Tribes</b> -Seth Godin</li>
-				<li><b><a href="books/never-eat-alone" target="_blank">Never Eat Alone</a></b> -Keith Ferrazzi</li>
-				<li><b>How to Win Friends and Influence People</b> -Dale Carnegie</li>
-			</ul>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_225664e6acb42af32191318f2_0615841a9b" tabindex="-1" value=""></div>
+		    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+		    </div>
+		</form>
 		</div>
-
-		<div class="books left">
-			<p><span class="underline">Misc. Others</span> (They're still great books!)</p>
-			<ul class="book-list">
-				<li><b>The Selfish Gene</b> -Ricard Dawkins</li>
-				<li><b><a href="books/in-defense-of-food" target="_blank">In Defense of Food</a></b> -Michael Pollan</li>
-				<li><b>Don't Make Me Think</b> -Steve Krug</li>
-				<li><b>Rocket Surgery Made Easy</b> -Steve Krug</li>
-			</ul>
-		</div>
-
-<!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-<form action="//justinmulvaney.us11.list-manage.com/subscribe/post?u=225664e6acb42af32191318f2&amp;id=0615841a9b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2>Want to be updated as the list grows? Join the mailing list!</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-</label>
-	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-</div>
-<div class="mc-field-group input-group">
-    <strong>Justin Mulvaney's Email List </strong>
-    <ul><li><input type="checkbox" value="1" name="group[13545][1]" id="mce-group[13545]-13545-0"><label for="mce-group[13545]-13545-0"> General Updates</label></li>
-<li><input type="checkbox" value="2" name="group[13545][2]" id="mce-group[13545]-13545-1"><label for="mce-group[13545]-13545-1"> Daily Blog</label></li>
-</ul>
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_225664e6acb42af32191318f2_0615841a9b" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-<!--End mc_embed_signup-->
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+		<!--End mc_embed_signup-->
 	</div>
 
 </body>
