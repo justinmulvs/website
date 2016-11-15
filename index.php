bb2f32102273f1ba97eecc9329626de51c7b035f
Justin <justin@crystalknows.com>
2016-11-15 12:12:07 -0600

    added favicon

--- index.php
+++ index.php
@@ -15,6 +15,9 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
 
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+
 	<!-- Google Analytics Tracking-->
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
